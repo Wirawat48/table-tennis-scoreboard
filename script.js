a39c8c0f10615124ddc974a593aae6e2b1305800
Wirawat48 <wirawthnw@gmail.com>
2025-07-24 23:19:51 +0700

    delete old scoreboard
    Signed-off-by: Wirawat48 <wirawthnw@gmail.com>

--- script.js
+++
@@ -1,6 +0,0 @@
-
-// ตัวอย่างพื้นฐาน ยังไม่เชื่อมแบบ realtime จนกว่าจะผูก Google Sheets
-document.getElementById('pointsA').textContent = 0;
-document.getElementById('pointsB').textContent = 0;
-document.getElementById('setsA').textContent = 0;
-document.getElementById('setsB').textContent = 0;
